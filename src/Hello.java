6351d84b92a66921a26b5289ee858154687691f2
Aditya Arora <adiarora0601@gmail.com>
2024-09-09 14:16:09 +0530

    Update

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(4+3);
+        System.out.println(3*6);
 
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 26;
+        int aGe = 12;
+        int marks = 98;
+
+        int secondMarksOfStudents = 23;
+        int second_allowed$allowed = 12;
+
+        int $marks = 12;
+
+
+        boolean isPassed = true;
+        int count = 12;
+        byte newMarks = 13;
+
+
+
+        System.out.println(count);
+        System.out.println(newMarks);
+        System.out.println(isPassed);
+
+
+
+
+    }
+}
